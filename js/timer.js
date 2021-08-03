86726a112c959274663f059ac6e7c54797c63610
Andrii Lypovetskyi <li_andr@ukr.net>
2021-08-03 19:45:40 +0300

    Переделал через class CountdownTimer

--- js/timer.js
+++ js/timer.js
@@ -2,77 +2,54 @@
 //   selector: "#timer-1",
 //   targetDate: new Date("Jul 17, 2019"),
 // });
-
-// /*
-//  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-//  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-//  */
-// const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-// /*
-//  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-//  * остатка % и делим его на количество миллисекунд в одном часе
-//  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-//  */
-// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-// /*
-//  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-//  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-//  */
-// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-// /*
-//  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-//  * миллисекунд в одной секунде (1000)
-//  */
-// const secs = Math.floor((time % (1000 * 60)) / 1000);
-const refs = {
-  secs: document.querySelector('span[data-value="secs"]'),
-  mins: document.querySelector('span[data-value="mins"]'),
-  hours: document.querySelector('span[data-value="hours"]'),
-  days: document.querySelector('span[data-value="days"]'),
-};
-
-const timer = {
-  targetDate: new Date("Aug 30, 2021"),
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.refs = {
+      secs: document.querySelector('span[data-value="secs"]'),
+      mins: document.querySelector('span[data-value="mins"]'),
+      hours: document.querySelector('span[data-value="hours"]'),
+      days: document.querySelector('span[data-value="days"]'),
+    };
+  }
   start() {
     setInterval(
       () => {
-        const time = this.targetDate - Date.now();
-        const { days, hours, mins, secs } = getTimeComponent(time);
+        const restTime = this.targetDate - Date.now();
+        const { days, hours, mins, secs } = this.getTimeComponent(restTime);
         console.log(days, ":", hours, ":", mins, ":", secs);
-        refs.days.textContent = days;
-        refs.hours.textContent = hours;
-        refs.mins.textContent = mins;
-        refs.secs.textContent = secs;
+        this.refs.days.textContent = days;
+        this.refs.hours.textContent = hours;
+        this.refs.mins.textContent = mins;
+        this.refs.secs.textContent = secs;
       },
       1000,
       1000
     );
-  },
-};
+  }
+  //Функция разбивки по дням : часам : минутам : секундам
+  getTimeComponent(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Aug 31, 2021"),
+});
 
 timer.start();
-
-function pad(value) {
-  return String(value).padStart(2, "0");
-}
-
-function differenceInTime() {
-  console.log(days, ":", hours, ":", mins, ":", secs);
-}
-
-function getTimeComponent(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  return { days, hours, mins, secs };
-}
