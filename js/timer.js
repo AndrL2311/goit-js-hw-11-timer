794de8968708b0fe4212ee2be020080ba07ad959
Andrii Lypovetskyi <li_andr@ukr.net>
2021-08-03 13:50:09 +0300

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,8 @@
-new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
-});
+// new CountdownTimer({
+//   selector: "#timer-1",
+//   targetDate: new Date("Jul 17, 2019"),
+// });
+
 // /*
 //  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
 //  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
@@ -26,3 +27,37 @@ new CountdownTimer({
 //  * миллисекунд в одной секунде (1000)
 //  */
 // const secs = Math.floor((time % (1000 * 60)) / 1000);
+const refs = {
+  secs: document.querySelector('span[data-value="secs"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  days: document.querySelector('span[data-value="days"]'),
+};
+
+let time = null;
+
+const targetDate = new Date("Aug 30, 2021");
+console.log(targetDate);
+
+setInterval(differenceInTime, 1000, 1000);
+
+function differenceInTime() {
+  // time = targetDate - new Date();
+  time = targetDate - Date.now();
+  // console.log(time);
+
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  console.log(days, ":", hours, ":", mins, ":", secs);
+}
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
